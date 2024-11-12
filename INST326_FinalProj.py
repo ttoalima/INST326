633bf2d4e7f64e0a72927d19f98532a18932048e
Jwu12330 <jwu12330@terpmail.umd.edu>
2024-11-12 18:42:43 -0500

    added sections, started break reminders/study tips, created tips file

--- INST326_FinalProj.py
+++ INST326_FinalProj.py
@@ -1,2 +1,16 @@
 # INST 326
 
+# Partner Matching
+
+# Study Session Scheduler
+
+# Progress Tracking + Goals
+
+# Break Reminders and Study Tips
+
+def break_reminders():
+    pass
+
+def study_tips(tip):
+    with open tip_file as fc
+    pass
