ae03ad4e2ee4b2cedf0af5975452007e20e61e50
Jwu12330 <jwu12330@terpmail.umd.edu>
2024-11-12 19:10:09 -0500

    added comments and docstring to study_tips function, functions runs

--- INST326_FinalProj.py
+++ INST326_FinalProj.py
@@ -1,4 +1,5 @@
 # INST 326
+import random
 
 # Partner Matching
 
@@ -11,6 +12,17 @@
 def break_reminders():
     pass
 
-def study_tips(tip):
-    with open tip_file as fc
-    pass
+def study_tips():
+    """returns a study tip to the user when run.
+    
+    Args: none
+    
+    Returns: random tip"""
+    tip_bank = [] #bank for text to go into
+    with open("tips") as fc: #open file with the tips
+        for tip in fc: 
+            tip_bank.append(tip.strip()) #adds to tip bank 
+    random_tip = random.choice(tip_bank) #randomly picks a tip
+    print(random_tip) #returns study tip
+
+
